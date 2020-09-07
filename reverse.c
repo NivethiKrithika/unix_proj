5df0b9b4d0e9384dc6e1d3cfc893ca36a63ee17e
NKrithika <53506588+NivethiKrithika@users.noreply.github.com>
2020-09-07 16:22:26 +0530

    Add files via upload

--- reverse.c
+++ reverse.c
@@ -2,20 +2,23 @@
 #include <malloc.h>
 #include<string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 void display();
 int insert_line(char**);
 int write_to_file();
 void read_file(char*);
 void free_list();
+int hard_linked(char*, char*);
+
 #define MAX_NUM_ARGUMENTS 3
 int main(int argc, char* argv[])
 {
     //int ret = 0;
     int stdoutput = 0;
-    printf("No of arguments is %d",argc);
-    if(argc > MAX_NUM_ARGUMENTS)
+    //printf("No of arguments is %d",argc);
+    if((argc > MAX_NUM_ARGUMENTS) || (argc == 0))
     {
-        fprintf(stderr,"Invalid numbe rof inputs\n");
+        fprintf(stderr,"usage: reverse <input> <output>\n");
 	exit(1);
     }
     if(argc == 1)
@@ -29,24 +32,29 @@ int main(int argc, char* argv[])
         }
 	write_to_file(stdoutput, "NULL");
 	free(line);
+	exit(0);
     }
-    if((argc == MAX_NUM_ARGUMENTS) && (strcmp(argv[1],argv[2]) == 0))
-    {
-	fprintf(stderr,"Input and output file names must differ\n");
-	exit(1);
+    if(argc == MAX_NUM_ARGUMENTS)
+    { 
+        if((strcmp(argv[1],argv[2]) == 0) || (hard_linked(argv[1],argv[2])))
+        {
+	    fprintf(stderr,"reverse: input and output file must differ\n");
+	    exit(1);
+        }
     }
+    
     if((argc == MAX_NUM_ARGUMENTS)||(argc == MAX_NUM_ARGUMENTS -1))
     {
 	FILE *fp;
 	fp = fopen(argv[1],"r");
 	if(fp == NULL)
 	{
-	    fprintf(stderr,"Cannot open the file to read");
+	    fprintf(stderr,"reverse: cannot open file '%s'\n", argv[1]);
 	    exit(1);
 	}
 	else
 	{   
-            printf("Entering");
+            //printf("Entering");
 	    char* read_line = NULL;
 	    size_t nchars = 0;
 	    while(getline(&read_line, &nchars, fp)!= -1)
@@ -57,7 +65,7 @@ int main(int argc, char* argv[])
 	    if(argc < MAX_NUM_ARGUMENTS)
 	    {
 		stdoutput = 1;
-		printf("sent out");
+		//printf("sent out");
 	    	write_to_file(stdoutput,"NULL");
 	    }
 	    else
@@ -66,12 +74,44 @@ int main(int argc, char* argv[])
 	    //read_file(argv[2]);
 	}
 	fclose(fp);
+	exit(0);
     }
     
 }
 
 void* list_head = NULL;
 void* list_tail = NULL;
+
+int hard_linked(char* file1,char* file2)
+{
+    FILE *fp1,*fp2;
+    fp1 = fopen(file1,"r");
+    fp2 = fopen(file2,"r");
+    if(fp1 == NULL)
+    {
+        fprintf(stderr,"reverse: cannot open file '%s'\n",file1);
+        exit(1);	
+    }
+    struct stat first,second;
+    int stat_first_file = lstat(file1, &first);
+    int stat_second_file = lstat(file2, &second);
+    if(fp2 != NULL)
+    {
+        if((stat_first_file != 0) || (stat_second_file != 0))
+        {
+            fprintf(stderr,"lsat failed\n");
+	    fclose(fp1);
+	    fclose(fp2);
+            exit(1);
+        }
+	fclose(fp2);
+    }
+    fclose(fp1);
+    if ((first.st_ino == second.st_ino))
+        return 1;
+    else
+        return 0;
+}
 
 typedef struct Lines_node Lines_node;
 struct Lines_node
@@ -87,15 +127,15 @@ int insert_line(char** input_line)
     char* new_string = malloc(strlen(*input_line)+1);
     if(new_string == NULL)
     {
-        fprintf(stderr,"Memory Allocation failed\n");
+        fprintf(stderr,"malloc failed\n");
 	exit(1);
     }
     strcpy(new_string,*input_line);
     Lines_node* lines_node =  (Lines_node*)malloc(sizeof(Lines_node));
-    printf("lines node is %p\n",lines_node);
+    //printf("lines node is %p\n",lines_node);
     if(lines_node == NULL)
     {
-	    fprintf(stderr,"Memory Allocation failed\n");
+	    fprintf(stderr,"malloc failed\n");
 	    exit(1);
     }
     lines_node->line = new_string;
@@ -128,7 +168,7 @@ int write_to_file(int stdoutput,char* file)
     	output_file = fopen(file,"w");
     	if(output_file == NULL)
     	{
-	    fprintf(stderr,"Cannot open the file to write\n");
+	    fprintf(stderr,"reverse: cannot open file '%s'\n",file);
 	    exit(1);
         }
     }
@@ -154,12 +194,12 @@ void free_list()
     {
 	Lines_node* temp = traverse;
 	traverse = traverse->next;
-	printf("freeing inside while  %p",temp->line);
-	printf("freeing inside while %p",temp);
+	//printf("freeing inside while  %p",temp->line);
+	//printf("freeing inside while %p",temp);
 	free(temp->line);
 	free(temp);
     }
-    printf("Done freeing\n");
+    //printf("Done freeing\n");
 }
 /*void display()
 {
@@ -189,3 +229,4 @@ void read_file(char* file)
 	printf("%s",read);
     }
 }*/
+
